9414edb5fd354b89d65880c6ce1d4f1e312cb0db
jean650 <buewambuinaj@csdmedu.ca>
2025-01-23 09:06:51 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+player.onChat("route", function () {
+    mobs.spawn(CHICKEN, pos(10, 8, 7))
+    agent.teleport(pos(2, 0, 0), EAST)
+    agent.setItem(STRIPPED_CHERRY_LOG, 1, 1)
+    for (let index = 0; index < 10; index++) {
+        agent.destroy(DOWN)
+        agent.place(DOWN)
+        agent.move(FORWARD, 1)
+        gameplay.setDifficulty(HARD)
+    }
+})
