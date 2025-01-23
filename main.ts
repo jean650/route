eff91e50399185f8112a42d1b76eca0aac3190b2
jean650 <buewambuinaj@csdmedu.ca>
2025-01-23 09:17:05 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-player.onChat("route", function () {
+player.onChat("rue", function (num1) {
     mobs.spawn(CHICKEN, pos(10, 8, 7))
     agent.teleport(pos(2, 0, 0), EAST)
     agent.setItem(STRIPPED_CHERRY_LOG, 1, 1)
